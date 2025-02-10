7d3aad6b510f77471e1ef7456e310f1439158139
Jash Bhalani <jashbhalani@wireless-10-148-93-205.public.utexas.edu>
2025-02-10 03:12:08 -0600

    T

--- db.py
+++ db.py
@@ -1,15 +1,12 @@
 import mysql.connector
 import os
-from dotenv import load_dotenv
-
-# Load environment variables from .env
-load_dotenv()
 
 def get_db_connection():
     conn = mysql.connector.connect(
         host=os.getenv("MYSQL_HOST"),
         user=os.getenv("MYSQL_USER"),
         password=os.getenv("MYSQL_PASSWORD"),
-        database=os.getenv("MYSQL_DATABASE")
+        database=os.getenv("MYSQL_DATABASE"),
+        port=int(os.getenv("MYSQL_PORT", 3306))  # Default to 3306
     )
     return conn
